644c2b92327ef11dade47e6f0e547b7962c4e52a
user <you@example.com>
2015-06-05 14:26:15 +0400

    frrrrrrrrrrrrrrrrrrrr

--- my_flaskr/config.py
+++ my_flaskr/config.py
@@ -1,2 +1,4 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+USERNAME = 'user'
+PASSWORD = '111111'
--- my_flaskr/views.py
+++ my_flaskr/views.py
@@ -1,5 +1,5 @@
 #rom app import app
-from flask import render_template
+from flask import render_template, request
 
 import sys
 sys.path.append('../../NMS')
@@ -32,4 +32,16 @@ def route(app):
                                posts=posts,
                                devices=context.devices)
 
-
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        error = None
+        if request.method == 'POST':
+            if request.form['username'] != app.config['USERNAME']:
+                error = 'Invalid username'
+            elif request.form['password'] != app.config['PASSWORD']:
+                error = 'Invalid password'
+            else:
+                session['logged_in'] = True
+                flash('You were logged in')
+                return redirect(url_for('show_entries'))
+        return render_template('login.html', error=error)
